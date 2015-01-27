775772f2c9917878532138466f30a25e2be0145a
rpastor <rpastor@example.com>
2015-01-27 16:37:30 +0100

    Formateo de la clase

--- SimpleProject/src/com/vector/git/tutorial/Run.java
+++ SimpleProject/src/com/vector/git/tutorial/Run.java
@@ -2,22 +2,22 @@ package com.vector.git.tutorial;
 
 public class Run {
 
-	private static void err ( ) {
-		System.out.println( "Run group|persona <ID>" );
+	private static void err() {
+		System.out.println("Run group|persona <ID>");
 	}
-	
-	public static void main ( String[] args ) {
-		if ( args.length == 0 ) {
-			err ( );
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			err();
 			return;
 		}
-		
-		if ( "persona".equals(args[0]) ) {
-			System.out.println( new Reader().getPersona( args[1] ) );
-		} else if ( "group".equals(args[0]) ) {
-			System.out.println( new Reader().getGroup( args[1] ) );
+
+		if ("persona".equals(args[0])) {
+			System.out.println(new Reader().getPersona(args[1]));
+		} else if ("group".equals(args[0])) {
+			System.out.println(new Reader().getGroup(args[1]));
 		} else {
-			err ( );
+			err();
 		}
 	}
 
